e09a43008fda388b0a8c1af8c709f0e3f8359a42
Alena Sharai <exultantislupus@gmail.com>
2024-02-23 17:49:01 +0400

    feat: add websocket server to http-server file

---
+++ src/http_server/index.ts
@@ -0,0 +1,29 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as http from 'http';
+import { WebSocket } from 'ws';
+
+export const httpServer = http.createServer(function (req, res) {
+  const __dirname = path.resolve(path.dirname(''));
+  const file_path =
+    __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
+  fs.readFile(file_path, function (err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+});
+
+const wss = new WebSocket.Server({ port: 3000 });
+
+wss.on('connection', (ws) => {
+  console.log('New client connected');
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
