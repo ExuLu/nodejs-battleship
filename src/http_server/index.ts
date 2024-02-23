f86de47b45360ca5d5061a9b61dec82c1661496a
Alena Sharai <exultantislupus@gmail.com>
2024-02-23 17:55:36 +0400

    fix: update dependencies and move index.ts file to src folder

--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -23,6 +23,10 @@ const wss = new WebSocket.Server({ port: 3000 });
 wss.on('connection', (ws) => {
   console.log('New client connected');
 
+  ws.on('message', (message) => {
+    console.log(message);
+  });
+
   ws.on('close', () => {
     console.log('Client disconnected');
   });
